57ece80f1ba280ba3aa96c4ed60e67aa3d01f416
matheusdoedev <matheusdoe.dev@gmail.com>
2021-11-25 15:47:39 -0300

    refactor: entities array updated in database configuration
    User entity added

--- src/shared/config/configuration.ts
+++ src/shared/config/configuration.ts
@@ -1,17 +1,18 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
+import { User } from 'src/modules/users/entities/user.entity';
+
 interface IConfiguration {
   database: TypeOrmModuleOptions;
 }
 
 export default (): IConfiguration => ({
   database: {
-    type: 'postgres',
     host: process.env.DB_HOST,
     port: Number(process.env.DB_PORT),
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    entities: [],
+    entities: [User],
   },
 });
