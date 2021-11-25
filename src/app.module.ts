a51d6c1887a5b9a9a99dbbaba028e8f21e056118
matheusdoedev <matheusdoe.dev@gmail.com>
2021-11-25 08:18:52 -0300

    refactor: sequelize and config modules configured

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,34 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+
 import { UserModule } from './modules/user/user.module';
 import { PhotoModule } from './modules/photo/photo.module';
 import { AlbumModule } from './modules/album/album.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import configuration from './config/configuration';
 
 @Module({
-  imports: [UserModule, PhotoModule, AlbumModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+    }),
+    SequelizeModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        dialect: configService.get('database.dialect'),
+        synchronize: configService.get('database.synchronize'),
+        host: configService.get('database.host'),
+        port: configService.get('database.port'),
+        username: configService.get('database.username'),
+        password: configService.get('database.password'),
+        database: configService.get('database.database'),
+        models: configService.get('database.models'),
+      }),
+      inject: [ConfigService],
+    }),
+    UserModule,
+    PhotoModule,
+    AlbumModule,
+  ],
 })
 export class AppModule {}
